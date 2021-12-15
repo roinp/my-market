ec68c8a3c66b9d51cf0b7b2a882d7c4d0437757b
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-15 18:58:03 +0400

    feat: header responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      spacing: {
-        '12xl': '128rem',
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+	theme: {
+		extend: {
+			spacing: {
+				'12xl': '128rem',
+
+			},
+			colors: {
+				'main-color': '#fec900',
+			},
+			width: {
+				'375': ' 375px',
+				'300': '300px',
+				'1200':'1200px'
+
+			},
+			maxWidth: {
+				'640': '640px',
+				'300': '300px',
+				'1200':'1200px'
+			}
+		},
+		fontFamily: {
+			'helvetica': ['helvetica-geo'],
+		}
+	},
+	plugins: [],
 }
