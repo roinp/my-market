125b052ec08352bfce08ea722a5a9d55a9689b42
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-15 23:45:22 +0400

    refactor: merge with old files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+		extend: {
+			spacing: {
+				'12xl': '128rem',
+			},
+			colors: {
+				'main-color': '#fec900',
+				'red-sales':'#ff3b30'
+			},
+			width: {
+				'375': ' 375px',
+				'300': '300px',
+				'1200':'1200px'
+
+			},
+			maxWidth: {
+				'640': '640px',
+				'300': '300px',
+				'1200':'1200px'
+			},
+			borderWidth: {
+				3:'3px'
+			  },
+		},
+		fontFamily: {
+			'helvetica': ['helvetica-geo'],
+		}
+	},
+	plugins: [],
 }
