8c2fcb15c176071e3c5d3e31c09449c982fe732e
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-16 17:32:10 +0400

    feat:categories and online shopping

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,33 @@ module.exports = {
 			},
 			colors: {
 				'main-color': '#fec900',
-				'red-sales':'#ff3b30'
+				'red-sales': '#ff3b30'
 			},
 			width: {
 				'375': ' 375px',
 				'300': '300px',
-				'1200':'1200px'
-
+				'1200': '1200px',
+				'176': '176px',
+				'590': '590px',
+				'nav-lg': 'calc(33% - 20px)',
+				'nav-xl': 'calc(25% - 20px)',
 			},
+			height: {
+				'320': '320px',
+				'600': '600px'
+			},
+
 			maxWidth: {
 				'640': '640px',
 				'300': '300px',
-				'1200':'1200px'
+				'1200': '1200px'
 			},
 			borderWidth: {
-				3:'3px'
-			  },
+				3: '3px'
+			},
+			fontSize: {
+				0: '0'
+			}
 		},
 		fontFamily: {
 			'helvetica': ['helvetica-geo'],
