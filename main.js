d1d9e441ebe63e36a3d8e29cff013aede8d1bc40
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-19 13:14:21 +0400

    feat:add some services

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './index.css';
 import "@glidejs/glide/src/assets/sass/glide.core.scss";
 import Glide from '@glidejs/glide'
 
-['.glide', '.glide-mobile','.glide-ads'].forEach(className => {
+['.glide', '.glide-mobile','.glide-ads','.glide-services'].forEach(className => {
 	new Glide(className, {
 		autoplay: true,
 		animationDuration: 2000,
