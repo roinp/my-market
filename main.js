ef1d2fced45cc8af22c52542696d520b8c2ec49e
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-20 00:14:00 +0400

    fix: fix some padding and margin

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './index.css';
 import "@glidejs/glide/src/assets/sass/glide.core.scss";
 import Glide from '@glidejs/glide'
 
-['.glide', '.glide-mobile','.glide-ads','.glide-services','.glide-ads-2','.glide-ads-3','.glide-ads-block','.stories','.stories-2'].forEach(className => {
+['.glide', '.glide-mobile','.glide-ads','.glide-services','.glide-ads-2','.glide-ads-3','.glide-ads-block','.glide-stories','.glide-stories-2'].forEach(className => {
 	new Glide(className, {
 		autoplay: true,
 		animationDuration: 2000,
