1e677693aa0dafdb6060d48847416cdce0ec65ef
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2021-12-22 13:41:27 +0400

    turn off some autoplay in carousel

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ import './index.css';
 import "@glidejs/glide/src/assets/sass/glide.core.scss";
 import Glide from '@glidejs/glide'
 
-['.glide', '.glide-mobile','.glide-ads','.glide-services','.glide-ads-2','.glide-ads-3','.glide-ads-block','.glide-stories','.glide-stories-2'].forEach(className => {
+['.glide', '.glide-mobile'].forEach(className => {
 	new Glide(className, {
 		autoplay: true,	
+		animationDuration: 5000,
+	}).mount();
+});
+
+['.glide-ads','.glide-services','.glide-ads-2','.glide-ads-3','.glide-ads-block','.glide-stories','.glide-stories-2'].forEach(className => {
+	new Glide(className, {
 		animationDuration: 5000,
 	}).mount();
 });
